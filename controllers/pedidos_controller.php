e8b866863792dadd8da92a143d600df521d07a2d
Paulo19961944 <paulo24041996@gmail.com>
2025-01-31 23:44:07 -0300

    feat: Adicionando a Tabela de Produtos Pedidos como FK

--- controllers/pedidos_controller.php
+++ controllers/pedidos_controller.php
@@ -1,6 +1,7 @@
 <?php 
 $path = $_SERVER["DOCUMENT_ROOT"]. "/ecommerce"; // Define o Caminho Principal
 include_once($path . '/models/pedidos.php'); // Importa a Classe Pedidos
+include_once($path . 'models/produtos_pedidos.php'); // Importa a Classe Produto Pedidos
 
 class PedidosController {
     // Adiciona um Pedido
@@ -13,11 +14,17 @@ class PedidosController {
             exit();
         }
 
-        // Instância da Classe Pedidos
+        // Instância da Classe Pedidos e Produto Pedidos
         $objpedido = new Pedidos(); 
+        $objprodutopedido = new ProdutosPedidos();
+
+        // Captura as Sessões do Produto ID e do Pedido ID
+        $produto_id = $_SESSION["produto_id"];
+        $pedido_id = $_SESSION["pedido_id"];
 
         // Se o Pedido foi cadastrado, então enviar para a página da lista de pedidos
         if ($objpedido->cadastrar()) {
+            $objprodutopedido->cadastrar($produto_id, $pedido_id); // Cadastra na Tabela Produtos_Pedidos
             header("Location: http://localhost/ecommerce/views/pedidos/lista_pedidos.php");
         }
     }
